77e1752c124ac4c5648ac5861fd29c290efee7a0
Eneko Amieva <enekoamieva@gmail.com>
2020-04-07 18:37:49 +0200

    Correct load

--- inc/slider-acf-block/js/slider.js
+++ inc/slider-acf-block/js/slider.js
@@ -1,10 +1,12 @@
 (function ($) {
-    $(document).ready.(function() {
-        ('.bxslider').bxSlider({
+
+    $(window).on('load', function () {
+
+        $('.bxslider').bxSlider({
             auto: true,
             stopAutoOnClick: true,
             pager: true,
             slideWidth: 700
         });
-    });
+    })
 })(jQuery);
